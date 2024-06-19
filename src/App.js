432f016beb7cb8384e957e5f2413b3fd5aadd5de
hainam <hainam2003bn@gmail.com>
2024-06-19 11:45:50 +0700

    TestPage.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { Suspense, useState } from 'react'
-import { HashRouter, Route, Routes, Navigate } from 'react-router-dom'
-import { CSpinner } from '@coreui/react'
-import './scss/style.scss'
+import React, { Suspense, useState } from 'react';
+import { HashRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { CSpinner } from '@coreui/react';
+import './scss/style.scss';
 
 // Containers
-const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
 
 // Pages
-const Login = React.lazy(() => import('./views/pages/Login'))
-const Register = React.lazy(() => import('./views/pages/Register'))
+const Login = React.lazy(() => import('./views/pages/Login'));
+const Register = React.lazy(() => import('./views/pages/Register'));
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem('isAuthenticated') === 'true');
+
+  const handleLogout = () => {
+    setIsAuthenticated(false);
+    localStorage.removeItem('isAuthenticated');
+  };
 
   return (
     <HashRouter>
@@ -19,11 +24,11 @@ const App = () => {
         <Routes>
           <Route path="/login" name="Login Page" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
           <Route path="/register" name="Register Page" element={<Register />} />
-          <Route path="*" name="Home" element={isAuthenticated ? <DefaultLayout /> : <Navigate to="/login" />} />
+          <Route path="*" name="Home" element={isAuthenticated ? <DefaultLayout handleLogout={handleLogout} /> : <Navigate to="/login" />} />
         </Routes>
       </Suspense>
     </HashRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index';
+import ManageTestPage from '../views/test/TestPage'; 
+import UserList from '../views/manage-user/UserList';
+import Dashboard from '../views/dashboard/Dashboard';
 
-const DefaultLayout = () => {
+const DefaultLayout = ({ handleLogout }) => {
   return (
     <div>
       <AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100">
-        <AppHeader />
+        <AppHeader handleLogout={handleLogout} />
         <div className="body flex-grow-1">
-          <AppContent />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/management/users" element={<UserList />} />
+            <Route path="/test" element={<ManageTestPage />} /> 
+          </Routes>
         </div>
         <AppFooter />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DefaultLayout
+export default DefaultLayout;
